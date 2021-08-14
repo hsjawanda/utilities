f191ceca8a487c54ee3bea69c84bec088dbec14d
Harshdeep S Jawanda <hsjawanda@gmail.com>
2021-08-14 15:10:29 +0530

    Finished merge

--- src/main/java/com/hsjawanda/utilities/base/Enums.java
+++ src/main/java/com/hsjawanda/utilities/base/Enums.java
@@ -5,16 +5,18 @@ package com.hsjawanda.utilities.base;
 
 import static com.hsjawanda.utilities.repackaged.commons.lang3.StringUtils.trimToEmpty;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
-/**
- * @author Harshdeep S Jawanda <hsjawanda@gmail.com>
- *
- */
-public final class Enums {
+import com.hsjawanda.utilities.collections.Lists;
+
+public class Enums {
+
+	private static final String DELIMITER = ", ";
 
 	private Enums() {
 	}
@@ -33,6 +35,53 @@ public final class Enums {
 		} catch (IllegalArgumentException e) {
 			return defaultValue;
 		}
+	}
+
+	public static <T extends Enum<T>> boolean isValidValue(Class<T> enumCls, String enumValue) {
+		try {
+			Enum.valueOf(enumCls, enumValue);
+			return true;
+		} catch (IllegalArgumentException | NullPointerException e) {
+			return false;
+		}
+	}
+
+	public static <T extends Enum<T>> boolean isNotValidValue(Class<T> enumCls, String enumValue) {
+		return !isValidValue(enumCls, enumValue);
+	}
+
+	public static <T extends Enum<T>> T valueOfOrDefault(Class<T> enumCls, String enumValue, T defaultValue) {
+		try {
+			return Enum.valueOf(enumCls, enumValue);
+		} catch (IllegalArgumentException | NullPointerException e) {
+			return defaultValue;
+		}
+	}
+
+	public static <T extends Enum<T>> List<T> valuesOf(@Nonnull Class<T> enumCls, List<String> enumValues,
+			boolean prepValues) throws NullPointerException, IllegalArgumentException {
+		Objects.requireNonNull(enumCls, "enumCls can't be null");
+		List<T> retVal = new ArrayList<>();
+		List<String> badVals = new ArrayList<>();
+		if (Lists.isEmpty(enumValues))
+			return Lists.empty();
+		for (String enumVal : enumValues) {
+			try {
+				retVal.add(Enum.valueOf(enumCls, prepValues ? trimToEmpty(enumVal).toUpperCase() : enumVal));
+			} catch (IllegalArgumentException | NullPointerException e) {
+				badVals.add(String.valueOf(enumVal));
+			}
+		}
+		if (Lists.isNotEmpty(badVals)) {
+			throw new IllegalArgumentException(String.format("List contained values invalid for enum %s: %s",
+					enumCls.getSimpleName(), String.join(DELIMITER, badVals)));
+		}
+		return retVal;
+	}
+
+	public static <T extends Enum<T>> List<T> valuesOf(@Nonnull Class<T> enumCls, List<String> enumValues)
+			throws NullPointerException, IllegalArgumentException {
+		return valuesOf(enumCls, enumValues, false);
 	}
 
 }
